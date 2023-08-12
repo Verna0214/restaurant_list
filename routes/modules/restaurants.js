894ef7d0e58ce01748b66c2126ff69a9d23861af
Verna0214 <yuhsichen0214@gmail.com>
2023-08-12 10:46:17 +0800

    feat: add edit function

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -39,6 +39,15 @@ router.get('/search', (req, res) => {
   }
 })
 
+// detail page
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
+
 // create
 router.post('/new', (req, res) => {
   const {name, name_en, category, image, location, phone, google_map, rating, description} = req.body
@@ -57,14 +66,27 @@ router.post('/new', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// detail
-router.get('/:id', (req, res) => {
+// edit
+router.put('/:id', (req, res) => {
   const id = req.params.id
-  Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name,
+      restaurant.name_en = name_en,
+      restaurant.category = category,
+      restaurant.image = image,
+      restaurant.location = location,
+      restaurant.phone = phone,
+      restaurant.google_map = google_map,
+      restaurant.rating = rating,
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
 })
+
 
 
 module.exports = router
