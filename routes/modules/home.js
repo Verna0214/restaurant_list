bb8931e16f4afe7c76cf5cf277ffa396c447d0e2
Verna0214 <yuhsichen0214@gmail.com>
2023-08-12 14:07:07 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,9 +4,29 @@ const Restaurant = require('../../models/restaurant')
 
 // index page
 router.get('/', (req, res) => {
+  const sort = req.query.sortType
+
   Restaurant.find()
     .lean()
-    .then((restaurants) => res.render('index', { restaurants }))
+    .then((allRestaurants) => {
+      let restaurants = [...allRestaurants]
+
+      restaurants.sort((a, b) => {
+        switch(sort) {
+          case 'asc':
+            return a.name_en < b.name_en ? -1 : 1
+          case 'desc':
+            return a.name_en > b.name_en ? -1 : 1
+          case 'category':
+            return a.category < b.category ? -1 : 1
+          case 'location':
+            return a.location < b.category ? -1 : 1
+          default:
+            return 0
+        }
+      })
+      res.render('index', { restaurants })
+    })
     .catch(error => console.log(error))
 })
 
