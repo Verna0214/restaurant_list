60373cb60bf13cbcf6af64921fb30df49be3d806
Verna0214 <yuhsichen0214@gmail.com>
2023-08-18 10:30:01 +0800

    feat: use express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 // 判斷執行環境再載入 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -11,7 +12,7 @@ const router = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 // set handlebars
 app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }))
@@ -22,6 +23,12 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 // use method-override
 app.use(methodOverride('_method'))
+// user express-session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}))
 // use router
 app.use(router)
 // start server
